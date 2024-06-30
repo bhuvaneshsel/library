401db164c7776f4ac9f6d19a353a4601b2c3617c
Bhuvanesh Selvaraj <bhuvanesh.sel@gmail.com>
2024-06-29 19:52:17 -0700

    Added javascript to store form inputs in book object

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const addBookButton = document.querySelector(".add-book");
 const modal = document.querySelector(".book-modal");
 const opacity =document.querySelector(".opacity");
 
+const titleInput = document.querySelector("#title");
+const authorInput = document.querySelector("#author");
+const pagesInput = document.querySelector("#pages");
+const descriptionInput= document.querySelector("#description");
+const submitButton = document.querySelector("#submit");
+
 addBookButton.addEventListener("click", () => {
     modal.classList.add("active");
     opacity.classList.add("active")
@@ -11,15 +17,24 @@ opacity.addEventListener("click", () => {
     modal.classList.remove("active");
     opacity.classList.remove("active");
 })
+
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, description, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.description = description;
     this.read = read;
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(book) {
+    myLibrary.push(book);
 
 }
+
+submitButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const book = new Book(titleInput.value, authorInput.value, pagesInput.value, descriptionInput.value, "reading");
+    addBookToLibrary(book);
+})
