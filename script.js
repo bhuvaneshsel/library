eb7f179f0f637484a60393edbd73fe39cd546ff6
Bhuvanesh Selvaraj <bhuvanesh.sel@gmail.com>
2024-06-29 19:41:33 -0700

    Made pop up for adding book

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
+const addBookButton = document.querySelector(".add-book");
+const modal = document.querySelector(".book-modal");
+const opacity =document.querySelector(".opacity");
 
+addBookButton.addEventListener("click", () => {
+    modal.classList.add("active");
+    opacity.classList.add("active")
+})
 
+opacity.addEventListener("click", () => {
+    modal.classList.remove("active");
+    opacity.classList.remove("active");
+})
 const myLibrary = [];
 
 function Book(title, author, pages, read) {
